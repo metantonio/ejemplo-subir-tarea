7790bf8133401f241316a142a56c38221875344d
metantonio <antonio_martinez88@hotmail.com>
2021-01-21 22:20:31 +0000

    agregado endpoint /todos

--- src/app.py
+++ src/app.py
@@ -1,5 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) #instancia del servidor Flask
+
+
+@app.route('/todos', methods=['GET']) #decorador
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+#se mostraria en gitpod en: https://3245-c181d190-54dc-4dba-a45a-442802d9cead.ws-us03.gitpod.io/todos
+
+
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
