529106338176e16ea42d5cf0ed92035a42b277f2
metantonio <antonio_martinez88@hotmail.com>
2021-01-22 21:53:40 +0000

    agregada función delete

--- src/app.py
+++ src/app.py
@@ -32,6 +32,7 @@ def add_new_todo():
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete: ",position)
+    todos.pop(position)
     return jsonify(todos)
 
 
