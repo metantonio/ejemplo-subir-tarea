9d68035b63402d27e56f4d8601dbb63a248563cd
metantonio <antonio_martinez88@hotmail.com>
2021-01-22 21:23:56 +0000

    metodo post agregado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__) #instancia del servidor Flask
 
@@ -9,10 +10,24 @@ todos = [
 
 @app.route('/todos', methods=['GET']) #decorador
 def hello_world():
+    #return "<h1>Hello!</h1>"
     return jsonify(todos)
 #se mostraria en gitpod en: https://3245-c181d190-54dc-4dba-a45a-442802d9cead.ws-us03.gitpod.io/todos
 
-
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    print("Incoming request with the following body", request_body)
+    if isinstance(decoded_object, list):
+        for task in decoded_object:
+            todos.append(task)
+    elif isinstance(decoded_object, dict):
+        todos.append(task)
+    else:
+        return "Error 400"
+    #return 'Response for the POST todo'
+    return jsonify(todos)
 
 
 # These two lines should always be at the end of your app.py file.
